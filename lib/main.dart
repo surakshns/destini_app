b41f6db64b71b773223dee7a51e0b1e27ebb8058
surakshns <surakshnss@gmail.com>
2020-12-01 16:09:57 +0530

    destini app

---
+++ lib/main.dart
@@ -0,0 +1,97 @@
+import 'package:destini_challenge_starting/story_brain.dart';
+import 'package:flutter/material.dart';
+
+
+void main() => runApp(Destini());
+
+class Destini extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData.dark(),
+      home: StoryPage(),
+    );
+  }
+}
+
+Storybrain storydata = Storybrain();
+
+class StoryPage extends StatefulWidget {
+  _StoryPageState createState() => _StoryPageState();
+}
+
+class _StoryPageState extends State<StoryPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage
+            (image: AssetImage('images/background.png'),
+            fit: BoxFit.cover,
+          ),
+
+        ),
+        padding: EdgeInsets.symmetric(vertical: 50.0, horizontal: 15.0),
+        constraints: BoxConstraints.expand(),
+        child: SafeArea(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Expanded(
+                flex: 12,
+                child: Center(
+                  child: Text(
+                   storydata.getStory(),
+                    style: TextStyle(
+                      fontSize: 25.0,
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                flex: 2,
+                child: FlatButton(
+                  onPressed: () {
+                    setState(() {
+                      storydata.nextStory(1);
+                    });
+                  },
+                  color: Colors.red,
+                  child: Text(
+                    storydata.getChoice1(),
+                    style: TextStyle(
+                      fontSize: 20.0,
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20.0,
+              ),
+              Expanded(
+                flex: 2,
+                child: Visibility(
+                  visible: storydata.buttonShouldBeVisible(),
+                  child: FlatButton(
+                    onPressed: () {
+                     setState(() {
+                       storydata.nextStory(2);
+                     });
+                    },
+                    color: Colors.blue,
+                    child: Text(
+                      storydata.getChoice2(),
+                      style: TextStyle(
+                        fontSize: 20.0,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/story.dart
@@ -0,0 +1,11 @@
+class Story{
+  String storyTitles;
+  String choice1s;
+  String choice2s;
+
+  Story({String storyTitle,String choice1,String choice2}){
+    storyTitles=storyTitle;
+    choice1s=choice1;
+    choice2s=choice2;
+  }
+}
